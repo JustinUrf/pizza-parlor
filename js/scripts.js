d5905801032f04b02c42501a41d92e71f3f087f6
Justin Lee <lee.justin001126@gmail.com>
2023-01-27 10:38:55 -0800

    add JS/tdd logic for contructor that creates new pizza's

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Bussiness logic for PizzaParlor object
 function PizzaParlor() {
   this.pizzas = {};
   this.currentId = 0;
@@ -12,28 +13,34 @@ PizzaParlor.prototype.addPizza = function(pizza) {
 PizzaParlor.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
-}
+};
 
 PizzaParlor.prototype.findId = function(id) {
   if (this.pizzas[id] !== undefined) {
     return this.contacts[id];
   }
   return false;
-}
+};
+
 PizzaParlor.prototype.deleteId = function(id) {
   if (this.pizzas[id] === undefined) {
     return false;
   }
   delete this.pizzas[id];
   return true;
+};
+
+//Bussiness Logic for making Individual Pizzas
+
+function Pizza(size, meatArray, veggieArray) {
+  this.size = size;
+  this.meatArray = meatArray;
+  this.veggieArray = veggieArray;
 }
 
 
-
-
 let pizzaParlor = new PizzaParlor();
-let emptyPizza = {size:"large", topping1:"mushrooms"}
-pizzaParlor.addPizza(emptyPizza)
 
-pizzaParlor.deleteId(1)
-console.log(pizzaParlor);
+let pizza = new Pizza("large", ["sausage", "pepperoni", "bacon", "chicken"], ["mushroom"])
+
+console.log(pizza)
